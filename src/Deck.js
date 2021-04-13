86001f6118ac0f76cff013dd0c436311e5ea9bfd
Matthew Eckes <eckesm@gmail.com>
2021-04-13 16:17:52 -0400

    Finsished part 2

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Card from './Card';
 import './Deck.css';
@@ -7,6 +7,7 @@ const Deck = ({ deckCount = 1 }) => {
 	const [ deckId, setDeckId ] = useState(null);
 	const [ cards, setCards ] = useState([]);
 	const [ remaining, setRemaining ] = useState(null);
+	const [ drawingCards, setDrawingCards ] = useState(false);
 
 	useEffect(
 		() => {
@@ -21,15 +22,31 @@ const Deck = ({ deckCount = 1 }) => {
 		[ deckCount ]
 	);
 
+	let timerId = useRef();
+
+	const startDrawingCards = () => {
+		setDrawingCards(true);
+		timerId.current = setInterval(() => {
+			drawCard();
+		}, 1000);
+	};
+
+	const stopDrawingCards = () => {
+		setDrawingCards(false);
+		clearInterval(timerId.current);
+	};
+
 	async function drawCard() {
 		const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1
     `);
 		const newCard = res.data.cards[0];
 		setRemaining(res.data.remaining);
-		// const leftOffset = (deckCount * 52 - res.data.remaining + 1) * 30;
 		const leftOffset = ((deckCount * 52 - res.data.remaining - 1) % 13) * 30 + 25;
 		const topOffset = (deckCount * 52 / 13 - Math.floor(res.data.remaining / 13)) * 90;
 		setCards(cards => [ ...cards, { ...newCard, leftOffset: leftOffset, topOffset: topOffset } ]);
+    if (res.data.remaining===0){
+      clearInterval(timerId.current)
+    }
 	}
 
 	const components = cards.map(card => (
@@ -48,9 +65,17 @@ const Deck = ({ deckCount = 1 }) => {
 		<div className="Deck">
 			{!deckId && <h3>...loading new deck...</h3>}
 			{deckId &&
-			remaining !== 0 && (
-				<button className="Deck-drawCard_button" onClick={drawCard}>
-					Draw a card from deck!
+			remaining !== 0 &&
+			!drawingCards && (
+				<button className="Deck-drawCard_button" onClick={startDrawingCards}>
+					Start Drawing Cards from Deck!
+				</button>
+			)}
+			{deckId &&
+			remaining !== 0 &&
+			drawingCards && (
+				<button className="Deck-drawCard_button" onClick={stopDrawingCards}>
+					Stop Drawing Cards from Deck!
 				</button>
 			)}
 			{deckId && remaining === 0 && <h3>There are no cards remaining!</h3>}
